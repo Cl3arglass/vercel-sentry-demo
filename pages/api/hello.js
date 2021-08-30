cd94968896a715a092f602ea5918bec3c6f7e15c
Cl3arglass <haltaffer@gmail.com>
2021-08-30 15:33:07 -0700

    update error test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,8 +4,9 @@ import { withSentry } from '@sentry/nextjs';
 const doAsyncWork = () => Promise.reject(new Error('API Test 1'));
 doAsyncWork();
 
-async function handler(req, res) {
-  res.status(500).json({ name: 'John Doe' });
-}
+const handler = async (req, res) => {
+  throw new Error("API throw error test");
+  res.status(200).json({ name: "John Doe" });
+};
 
 export default withSentry(handler);
