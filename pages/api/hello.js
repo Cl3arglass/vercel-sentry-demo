0e4433601bb9b5f6a02149cc65c1ca4e8a0f6f24
Cl3arglass <haltaffer@gmail.com>
2021-08-30 15:23:30 -0700

    api error test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,7 +5,7 @@ const doAsyncWork = () => Promise.reject(new Error('API Test 1'));
 doAsyncWork();
 
 async function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' });
+  res.status(500).json({ name: 'John Doe' });
 }
 
 export default withSentry(handler);
